e4fc929c9823ff8b955039e714b9a5efcfd1050e
Faliell <76968922+Faliell@users.noreply.github.com>
2022-05-08 09:06:38 +0200

    bcrypt version

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import express from "express"
 import bodyParser from "body-parser"
 import ejs from "ejs"
 import mongoose from "mongoose"
-import md5 from "md5"
+import bcrypt from "bcrypt"
 
-
+const saltRounds = 10;
 
 const app = express()
 
-console.log(process.env.SECRET);
+
 
 app.use(express.static("public"))
 app.set("view engine","ejs")
@@ -38,32 +38,40 @@ app.get("/register", (req,res)=> {
 })
 
 app.post("/register", (req, res)=>{
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  })
 
-  newUser.save((err)=>{
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets")
-    }
-  })
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    })
+
+    newUser.save((err)=>{
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets")
+      }
+    })
+})
+
+
 })
 
 app.post("/login", (req, res)=>{
   const username = req.body.username
-  const password = md5(req.body.password)
+  const password = req.body.password
 
   User.findOne({email:username}, (err, foundUser)=>{
     if(err){
       console.log();
     }else{
       if(foundUser){
-        if (foundUser.password = password){
-          res.render("secrets")
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result === true){
+              res.render("secrets")
+            }
+});
       }
     }
   })
